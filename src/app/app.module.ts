e9cccbbc3545bed656c6e0cfb025b4d409ff7dd0
roshmaep <roshmaep1999@gmail.com>
2022-11-29 15:38:33 +0530

    productview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPhotoComponent } from './view-photo/view-photo.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+
+  {
+
+    path:"",
+
+    component:ViewPhotoComponent
+
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPhotoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
